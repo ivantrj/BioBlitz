8f8b213842600f4c192f6c02b1a4c8c8535d6291
Ivan Trj <ivantrajanovski@gmail.com>
2023-04-10 16:38:06 +0200

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Ivan Trajanovski on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
