005b2e56bdb8c6398c8332994d814a641b25455b
Ivan Trj <ivantrajanovski@gmail.com>
2023-04-10 17:17:24 +0200

    skeleton

--- BioBlitz/ContentView.swift
+++ BioBlitz/ContentView.swift
@@ -10,17 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("GREEN: 0")
+                
+                Spacer()
+                
+                Text("BIOBLITZ")
+                
+                Spacer()
+                
+                Text("RED: 0")
+            }
+            .font(.system(size: 36).weight(.black))
+            
+            VStack {
+                ForEach(0..<11, id: \.self) { row in
+                    HStack {
+                        ForEach(0..<22, id: \.self) { col in
+                            Text("X")
+                        }
+                    }
+                }
+            }
+            .padding()
+            .fixedSize()
+            .preferredColorScheme(.dark)
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
